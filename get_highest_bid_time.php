ad93ef7db747a4fa5e579017a127a52a494e3e45
MichaelsWork <96271673+MichaelsWork@users.noreply.github.com>
2023-04-24 00:38:30 +0000

    fixed get_highest_bid works

--- get_highest_bid_time.php
+++ get_highest_bid_time.php
@@ -2,22 +2,18 @@
 include("dbconfig.php");
 error_reporting(E_ALL|E_STRICT);
 
-$car_id = $_GET("car_id");
+$car_id = $_POST["car_id"];
 
 $con = mysqli_connect($host,$username, $password, $dbname)
         or die("Cannot connect to DB");
 
 
-$sql = "SELECT MAX(amount) AS highest_bid, user_id FROM BidNDriveDB.Bids WHERE car_id = $car_id";
+$sql = "SELECT MAX(bid_amount) AS highest_bid, user_id FROM .Bids WHERE car_id = '$car_id'";
 $result = mysqli_query($con, $sql);
-$row = mysqli_fetch_assoc($result);
+$row = mysqli_fetch_array($result);
 $highest_bid = $row["highest_bid"];
+echo"$highest_bid";
 
-
-$sql = "SELECT end_time from BidNDriveDB.Cars WHERE car_id = $car_id";
-$result = mysqli_query($con, $sql);
-$row = mysqli_fetch_assoc($result);
-$end_time = $row["end_time"];
 
 mysqli_close($con);
 ?>
